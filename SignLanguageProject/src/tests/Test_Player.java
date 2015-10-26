dbe75f7f566bcb7b0128d6d4687fe2e25672e1c1
eman <eman@le-metropolitain.ecs.vuw.ac.nz>
2015-10-27 11:18:03 +1300

    testing for video

---
+++ SignLanguageProject/src/tests/Test_Player.java
@@ -0,0 +1,57 @@
+package tests;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+public class Test_Player {
+
+    public static void main(final String[] args) {
+        NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), "C:\\Program Files\\VideoLAN\\VLC");
+        Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Test_Player(args);
+            }
+        });
+    }
+
+    private Test_Player(String[] args) {
+        JFrame frame = new JFrame("vlcj Tutorial");
+
+        MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
+
+        Canvas c = new Canvas();
+        c.setBackground(Color.black);
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.add(c, BorderLayout.CENTER);
+        frame.add(p, BorderLayout.CENTER);
+
+
+        EmbeddedMediaPlayer mediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer();
+
+        mediaPlayer.setVideoSurface(mediaPlayerFactory.newVideoSurface(c));
+        frame.setLocation(100, 100);
+        frame.setSize(1050, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+        mediaPlayer.playMedia("wellington.mp4");
+    }
+}
